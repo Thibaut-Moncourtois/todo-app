63498325cf789ded6734b13a60883956880d8c2a
Thibaut <thibaut.moncourtois@gmail.com>
2024-01-20 14:40:35 +0100

    setup route done

--- backend/src/routes/tasksRouter.js
+++ backend/src/routes/tasksRouter.js
@@ -2,9 +2,29 @@ import express from "express";
 const router = express.Router();
 
 const tasksRouter = (db) => {
+  // =====> ROUTE GET <=====
+
   router.get("/", (req, res) => {
-    res.send("Get all tasks");
+    const sql = "SELECT * FROM tasks";
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        return res.status(400).json({ error: err.message });
+      }
+      res.json(rows);
+    });
   });
+
+  router.get("/:id", (req, res) => {
+    const sql = "SELECT * FROM tasks WHERE id = ?";
+    db.get(sql, [req.params.id], (err, row) => {
+      if (err) {
+        return res.status(400).json({ error: err.message });
+      }
+      res.json(row);
+    });
+  });
+
+  // =====> ROUTE POST <=====
 
   router.post("/", (req, res) => {
     const { title, description } = req.body;
@@ -14,6 +34,31 @@ const tasksRouter = (db) => {
         return res.status(400).json({ error: err.message });
       }
       res.json({ id: this.lastID });
+    });
+  });
+
+  // =====> ROUTE PUT <=====
+
+  router.put("/:id", (req, res) => {
+    const { title, description } = req.body;
+    const sql = "UPDATE tasks SET title = ?, description = ? WHERE id = ?";
+    db.run(sql, [title, description, req.params.id], function (err) {
+      if (err) {
+        return res.status(400).json({ error: err.message });
+      }
+      res.json({ changes: this.changes });
+    });
+  });
+
+  // =====> ROUTE DELETE <=====
+
+  router.delete("/:id", (req, res) => {
+    const sql = "DELETE FROM tasks WHERE id = ?";
+    db.run(sql, [req.params.id], function (err) {
+      if (err) {
+        return res.status(400).json({ error: err.message });
+      }
+      res.json({ deleted: this.changes });
     });
   });
 

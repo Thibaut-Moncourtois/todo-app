fa986eb37047e5c0c3e1c1fa8f485003565b2963
Thibaut <thibaut.moncourtois@gmail.com>
2024-01-20 13:31:32 +0100

    setup first route

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import sqlite3 from "sqlite3";
+import tasks from "./src/routes/tasks";
 const app = express();
-const port = 4500;
+const port = 5173;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/tasks", tasks);
 
 let db = new sqlite3.Database("./tododb.db", (err) => {
   if (err) {
